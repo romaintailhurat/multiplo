ebe58b82ef8beb049727bc10636b40ff80a912d4
romaintailhurat <romain.tailhurat@gmail.com>
2016-12-22 14:05:38 +0100

    Adding basic routing

--- App.elm
+++ App.elm
@@ -38,15 +38,19 @@ displayMult model =
 
 -- Model
 
+type Page =
+  HomePage | MultPage
+
 type alias Model =
   { x : Int
   , y : Int
   , res : Int
-  , visibility : Bool}
+  , visibility : Bool
+  , page : Page}
 
 init : (Model, Cmd Msg)
 init =
-  (Model 1 1 1 False, Cmd.none)
+  (Model 1 1 1 False HomePage, Cmd.none)
 
 -- Update
 
@@ -54,6 +58,8 @@ type Msg =
   Gen
   | New (Int, Int)
   | ShowRes
+  | Home
+  | ShowMult
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -61,9 +67,13 @@ update msg model =
     Gen ->
       (model, Random.generate New randomPair)
     New pair ->
-      (Model (first pair) (second pair) (multiplication pair) False, Cmd.none)
+      (Model (first pair) (second pair) (multiplication pair) False MultPage, Cmd.none)
     ShowRes ->
       ({ model | visibility = True }, Cmd.none)
+    Home ->
+      ({ model | page = HomePage }, Cmd.none)
+    ShowMult ->
+      ({ model | page = MultPage }, Cmd.none)
 
 -- Subscriptions
 
@@ -73,23 +83,39 @@ subscriptions model =
 
 -- Views
 
-view : Model -> Html Msg
-view model =
+homeView : Model -> Html Msg
+homeView model =
   div []
     [ stylesheet "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css"
     , div [ class "container" ]
-      [ div [ style [("font-size", "20em")] ]
-        [ text (displayMult model) ]
-      , div []
-        [ button [ class "button is-primary", onClick Gen ]
-          [ text "Nouvelle multiplication" ]
-        , button [ class "button is-primary", onClick ShowRes ]
-          [ text "Montrer résultat"]]
-      , div []
-        [ a [ href "https://github.com/romaintailhurat/multiplo" ]
-          [ text "[ source code ]"] ]
-      ]
+      [ text "Hello", button [ class "button is-primary", onClick ShowMult ] [ text "Mult ?"]]
     ]
+
+multView : Model -> Html Msg
+multView model =
+   div []
+     [ stylesheet "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css"
+     , div [ class "container" ]
+       [ div [ style [("font-size", "20em")] ]
+         [ text (displayMult model) ]
+       , div []
+         [ button [ class "button is-primary", onClick Gen ]
+           [ text "Nouvelle multiplication" ]
+         , button [ class "button is-primary", onClick ShowRes ]
+           [ text "Montrer résultat"]]
+       , div []
+         [ a [ href "https://github.com/romaintailhurat/multiplo" ]
+           [ text "[ source code ]"] ]
+       ]
+     ]
+
+view : Model -> Html Msg
+view model =
+  case model.page of
+    HomePage ->
+      homeView model
+    MultPage ->
+      multView model
 
 main : Program Never Model Msg
 main =
